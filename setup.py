bd9790b920da0643caddb322c54c65490c748d7d
John Carr <john.carr@unrouted.co.uk>
2018-07-22 16:57:05 +0100

    Add a description

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='pytest-docker-tools',
     version='0.0.2.dev0',
+    description='py.test fixture factories for Docker integration testing',
     packages = ['pytest_docker_tools'],
     entry_points = {
         'pytest11': [
