29ec9b6e19115c221187a39f98148e785775cff0
John Carr <john.carr@unrouted.co.uk>
2018-07-22 22:29:35 +0100

    Add pyproject.toml

--- pytest_docker_tools/__init__.py
+++ pytest_docker_tools/__init__.py
@@ -1,5 +1,12 @@
+'''
+A set of tools for creating declarative docker py.test integration fixtures.
+'''
+
 from . import factories
 from .utils import wait_for_callable
+
+
+__version__ = '0.0.2'
 
 
 __all__ = [
