4dd9b8b6fe462680534c8208510dae3710eecad5
John Carr <john.carr@unrouted.co.uk>
2018-07-27 11:57:35 +0100

    Pull template handling out of container specific code

--- pytest_docker_tools/factories/container.py
+++ pytest_docker_tools/factories/container.py
@@ -1,8 +1,10 @@
-import inspect
 import textwrap
-from string import Formatter
 
 import pytest
+from pytest_docker_tools.templates import (
+    find_fixtures_in_params,
+    resolve_fixtures_in_params,
+)
 from pytest_docker_tools.utils import wait_for_callable
 from pytest_docker_tools.wrappers import Container
 
@@ -14,73 +16,6 @@ def create_container(request, docker_client, *args, **kwargs):
     request.addfinalizer(lambda: container.remove(force=True) and container.wait(timeout=10))
 
     return Container(container)
-
-
-class FixtureCollector(object):
-
-    def visit_value(self, val):
-        if isinstance(val, str):
-            for literal_text, format_spec, conversion, _ in Formatter().parse(val):
-                if format_spec:
-                    yield format_spec.split('.')[0].split('[')[0]
-        elif callable(val):
-            yield from inspect.getargspec(val)[0]
-
-    def visit_list(self, val):
-        for v in val:
-            yield from self.visit(v)
-
-    def visit_dict(self, mapping):
-        for k, v in mapping.items():
-            yield from self.visit(k)
-            yield from self.visit(v)
-
-    def visit(self, value):
-        if isinstance(value, dict):
-            yield from self.visit_dict(value)
-        elif isinstance(value, list):
-            yield from self.visit_list(value)
-        elif value:
-            yield from self.visit_value(value)
-
-    def get_fixtures_from_params(self, kwargs):
-        return set(self.visit(kwargs))
-
-
-class ConstructorRenderer(object):
-
-    def __init__(self, request):
-        self.request = request
-
-    def visit_value(self, val):
-        if isinstance(val, str):
-            return FixtureFormatter(self.request).format(val)
-        elif callable(val):
-            return val(*[self.request.getfixturevalue(f) for f in inspect.getargspec(val)[0]])
-        return val
-
-    def visit_list(self, val):
-        return [self.visit(v) for v in val]
-
-    def visit_dict(self, mapping):
-        return {self.visit(k): self.visit(v) for (k, v) in mapping.items()}
-
-    def visit(self, value):
-        if isinstance(value, dict):
-            return self.visit_dict(value)
-        elif isinstance(value, list):
-            return self.visit_list(value)
-        elif value:
-            return self.visit_value(value)
-
-
-class FixtureFormatter(Formatter):
-
-    def __init__(self, request):
-        self.request = request
-
-    def get_value(self, key, args, kwargs):
-        return self.request.getfixturevalue(key)
 
 
 def build_fixture_function(name, docstring, fixtures, callable):
@@ -117,7 +52,7 @@ def container(*, scope='function', **kwargs):
         container = create_container(
             request,
             docker_client,
-            **ConstructorRenderer(request).visit(local_kwargs)
+            **resolve_fixtures_in_params(request, local_kwargs)
         )
 
         wait_for_callable(
@@ -127,7 +62,7 @@ def container(*, scope='function', **kwargs):
 
         return container
 
-    fixtures = FixtureCollector().get_fixtures_from_params(kwargs).union(set(('request', 'docker_client')))
+    fixtures = find_fixtures_in_params(kwargs).union(set(('request', 'docker_client')))
 
     container = build_fixture_function(
         'container',
