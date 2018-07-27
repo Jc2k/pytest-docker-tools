a7435a6381868c574f47eac11880d614f294e1a3
John Carr <john.carr@unrouted.co.uk>
2018-07-27 09:14:27 +0100

    Add a visitor that can extract a list of fixtures used during test setup

--- pytest_docker_tools/factories/container.py
+++ pytest_docker_tools/factories/container.py
@@ -15,6 +15,64 @@ def create_container(request, docker_client, *args, **kwargs):
     return Container(container)
 
 
+class FixtureCollector(object):
+
+    def visit_value(self, val):
+        if isinstance(val, str):
+            for literal_text, format_spec, conversion, _ in Formatter().parse(val):
+                if format_spec:
+                    yield format_spec.split('.')[0].split('[')[0]
+        elif callable(val):
+            yield from inspect.getargspec(val)[0]
+
+    def visit_list(self, val):
+        for v in val:
+            yield from self.visit(v)
+
+    def visit_dict(self, mapping):
+        for k, v in mapping.items():
+            yield from self.visit(k)
+            yield from self.visit(v)
+
+    def visit(self, value):
+        if isinstance(value, dict):
+            yield from self.visit_dict(value)
+        elif isinstance(value, list):
+            yield from self.visit_list(value)
+        elif value:
+            yield from self.visit_value(value)
+
+    def get_fixtures_from_params(self, kwargs):
+        return tuple(set(self.visit(kwargs)))
+
+
+class ConstructorRenderer(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    def visit_value(self, val):
+        if isinstance(val, str):
+            return FixtureFormatter(self.request).format(val)
+        elif callable(val):
+            return val(*[self.request.getfixturevalue(f) for f in inspect.getargspec(val)[0]])
+        return val
+
+    def visit_list(self, val):
+        return [self.visit(v) for v in val]
+
+    def visit_dict(self, mapping):
+        return {self.visit(k): self.visit(v) for (k, v) in mapping.items()}
+
+    def visit(self, value):
+        if isinstance(value, dict):
+            return self.visit_dict(value)
+        elif isinstance(value, list):
+            return self.visit_list(value)
+        elif value:
+            return self.visit_value(value)
+
+
 class FixtureFormatter(Formatter):
 
     def __init__(self, request):
@@ -22,31 +80,6 @@ class FixtureFormatter(Formatter):
 
     def get_value(self, key, args, kwargs):
         return self.request.getfixturevalue(key)
-
-
-def _process_val(request, val):
-    if isinstance(val, str):
-        return FixtureFormatter(request).format(val)
-    elif callable(val):
-        return val(*[request.getfixturevalue(f) for f in inspect.getargspec(val)[0]])
-    return val
-
-
-def _process_list(request, val):
-    return [_process(request, v) for v in val]
-
-
-def _process_dict(request, mapping):
-    return {_process(request, k): _process(request, v) for (k, v) in mapping.items()}
-
-
-def _process(request, val):
-    if isinstance(val, dict):
-        return _process_dict(request, val)
-    elif isinstance(val, list):
-        return _process_list(request, val)
-    else:
-        return _process_val(request, val)
 
 
 def container(*, scope='function', **kwargs):
@@ -67,7 +100,7 @@ def container(*, scope='function', **kwargs):
         container = create_container(
             request,
             docker_client,
-            **_process_dict(request, local_kwargs)
+            **ConstructorRenderer(request).visit(local_kwargs)
         )
 
         wait_for_callable(
@@ -78,5 +111,7 @@ def container(*, scope='function', **kwargs):
         return container
 
     pytest.fixture(scope=scope)(container)
+
+    # pytest.mark.usefixtures(*FixtureCollector().get_fixtures_from_params(kwargs))(container)
 
     return container
