a52b727cb2070a16af19e75fec54b894aa1bc2c4
John Carr <john.carr@unrouted.co.uk>
2018-07-30 11:13:32 +0100

    0.0.6

--- pytest_docker_tools/__init__.py
+++ pytest_docker_tools/__init__.py
@@ -5,7 +5,7 @@ An opionated set of helpers for defining Docker integration test environments wi
 from .factories import build, container, fetch, network, volume
 
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 __all__ = [
