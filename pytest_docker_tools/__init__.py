f06ebe1f2865e44158fde045cdb05bd2e226a040
John Carr <john.carr@unrouted.co.uk>
2018-07-30 18:17:09 +0100

    Version bump

--- pytest_docker_tools/__init__.py
+++ pytest_docker_tools/__init__.py
@@ -5,7 +5,7 @@ An opionated set of helpers for defining Docker integration test environments wi
 from .factories import build, container, fetch, network, volume
 
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 
 __all__ = [
