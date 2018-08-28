dcfda5f8adb72cae4428f36dcedf802d57ceeaac
John Carr <john.carr@unrouted.co.uk>
2018-08-28 12:41:49 +0100

    Missing file

---
+++ pytest_docker_tools/exceptions.py
@@ -0,0 +1,17 @@
+class TimeoutError(Exception):
+    pass
+
+
+class ContainerError(Exception):
+
+    def __init__(self, container, *args, **kwargs):
+        self._container = container
+        super().__init__(*args, **kwargs)
+
+
+class ContainerFailed(ContainerError):
+    pass
+
+
+class ContainerNotReady(ContainerError):
+    pass
