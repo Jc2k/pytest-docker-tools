8c26e0083200572d718f8dbd2dd83901a1e678ec
Alexander Petrenz <alexander.petrenz@peerox.de>
2021-05-25 21:51:38 +0200

    now testing image fixture_factory

--- tests/integration/test_container.py
+++ tests/integration/test_container.py
@@ -3,10 +3,11 @@ import socket
 
 import pytest
 
-from pytest_docker_tools import build, container, fetch
+from pytest_docker_tools import build, container, fetch, image
 from pytest_docker_tools.utils import wait_for_callable
 
 test_container_1_image = fetch(repository="redis:latest")
+test_container_1_same_image = image(name="redis:latest")
 
 test_container_1 = container(
     image="{test_container_1_image.id}",
@@ -16,7 +17,15 @@ test_container_1 = container(
     name="test_container",
 )
 
-reused_container = container(name="test_container")
+original_container_1 = container(
+    image="{test_container_1_same_image.id}",
+    ports={
+        "6379/tcp": None,
+    },
+    name="test_container_org",
+)
+
+reused_container = container(name="test_container_org")
 
 ipv6_folder = os.path.join(os.path.dirname(__file__), "fixtures/ipv6")
 ipv6_image = build(path=ipv6_folder)
@@ -67,9 +76,9 @@ def test_container_label(docker_client, test_container_1):
 
 
 def test_container_reuse_create(
-    enable_container_reuse, docker_client, test_container_1, reused_container
+    enable_container_reuse, docker_client, original_container_1, reused_container
 ):
-    assert test_container_1.id == reused_container.id
+    assert original_container_1.id == reused_container.id
     for c in docker_client.containers.list(ignore_removed=True):
-        if c.id == test_container_1.id:
+        if c.id == original_container_1.id:
             c.remove(force=True)
