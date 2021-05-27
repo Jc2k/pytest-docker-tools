94a441914a4d2278f5df940fc4f04219957cbdf1
John Carr <john.carr@unrouted.co.uk>
2021-05-27 17:29:35 +0100

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@ VERSION=${1:-patch}
 
 poetry version $VERSION
 git commit -a -m "Version bump"
-git tag -a -s `poetry version | awk '{ print $2; }'`
+git tag -a `poetry version | awk '{ print $2; }'`
 git push --tags
 git push
 
