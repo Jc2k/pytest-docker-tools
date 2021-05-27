38d357acf7089f8e33baa9e7f40c0f8886cc9297
Alexander Petrenz <alexander.petrenz@peerox.de>
2021-05-27 23:04:25 +0200

    black fix

--- tests/integration/test_network.py
+++ tests/integration/test_network.py
@@ -13,7 +13,9 @@ def test_network_1_created(docker_client, test_network_1):
         assert False, "Looks like we failed to create a network"
 
 
-def test_reusable_network_2_created(enable_container_reuse, docker_client, test_network_2):
+def test_reusable_network_2_created(
+        enable_container_reuse, docker_client, test_network_2
+):
     for n in docker_client.networks.list():
         if n.id == test_network_2.id:
             # Looks like we managed to start one!
