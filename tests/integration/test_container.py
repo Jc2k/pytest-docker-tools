5bddcdb089e3f6d16a70d5a82f8e6a61c8c403a7
John Carr <john.carr@unrouted.co.uk>
2021-05-29 15:47:53 +0100

    Assert containers are reused

--- tests/integration/test_container.py
+++ tests/integration/test_container.py
@@ -1,8 +1,15 @@
+import logging
 import os
 import socket
 
+from _pytest.pytester import Pytester
+from docker.errors import NotFound
+import pytest
+
 from pytest_docker_tools import build, container, fetch, image
 from pytest_docker_tools.utils import LABEL_REUSABLE_CONTAINER, wait_for_callable
+
+logger = logging.getLogger(__name__)
 
 test_container_1_image = fetch(repository="redis:latest")
 test_container_1_same_image = image(name="redis:latest")
@@ -70,3 +77,58 @@ def test_container_reuse_create(
     for c in docker_client.containers.list(ignore_removed=True):
         if c.id == original_container_1.id:
             c.remove(force=True)
+
+
+def test_reusable_reused(request, pytester: Pytester, docker_client):
+    def _cleanup():
+        try:
+            container = docker_client.containers.get("my-reusable-container")
+        except NotFound:
+            return
+        container.remove(force=True)
+
+    with pytest.raises(NotFound):
+        docker_client.containers.get("my-reusable-container")
+
+    request.addfinalizer(_cleanup)
+
+    pytester.makeconftest(
+        "\n".join(
+            (
+                "from pytest_docker_tools import container, fetch",
+                "memcache_image = fetch(repository='memcached:latest')",
+                "memcache = container(",
+                "    name='my-reusable-container',",
+                "    image='{memcache_image.id}',",
+                "    scope='session',",
+                "    ports={",
+                "        '11211/tcp': None,",
+                "    },",
+                ")",
+            )
+        )
+    )
+
+    pytester.makepyfile(
+        test_reusable_container="\n".join(
+            (
+                "import socket",
+                "def test_session_1(memcache):",
+                "    sock = socket.socket()",
+                "    sock.connect(('127.0.0.1', memcache.ports['11211/tcp'][0]))",
+                "    sock.close()",
+            )
+        )
+    )
+
+    result = pytester.runpytest("--reuse-containers")
+    result.assert_outcomes(passed=1)
+
+    run1 = docker_client.containers.get("my-reusable-container")
+
+    result = pytester.runpytest("--reuse-containers")
+    result.assert_outcomes(passed=1)
+
+    run2 = docker_client.containers.get("my-reusable-container")
+
+    assert run1.id == run2.id
