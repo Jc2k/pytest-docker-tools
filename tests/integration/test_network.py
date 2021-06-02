e2999b0a79c95b9f1fcf709b43afb65b6ca3a0a4
John Carr <john.carr@unrouted.co.uk>
2021-06-03 00:17:25 +0100

    Add test for conflicting container when removing stale network

--- tests/integration/test_network.py
+++ tests/integration/test_network.py
@@ -338,3 +338,78 @@ def test_reusable_stale_dependent_container(
 
     with pytest.raises(NotFound):
         run1.reload()
+
+
+def test_reusable_conflicting_container(
+    request, pytester: Pytester, docker_client: DockerClient
+):
+    def _cleanup():
+        try:
+            container = docker_client.containers.get(
+                "test_reusable_conflicting_container_net"
+            )
+            container.remove(force=True)
+        except NotFound:
+            pass
+
+        try:
+            network = docker_client.networks.get("test_reusable_conflicting_container")
+            network.remove()
+        except NotFound:
+            pass
+
+    request.addfinalizer(_cleanup)
+
+    with pytest.raises(NotFound):
+        docker_client.networks.get("test_reusable_conflicting_container")
+        docker_client.containers.get("test_reusable_conflicting_container_net")
+
+    pytester.makeconftest(
+        "\n".join(
+            (
+                "from pytest_docker_tools import container, fetch, network",
+                "redis_network = network(",
+                "    name='test_reusable_conflicting_container',",
+                ")",
+            )
+        )
+    )
+
+    pytester.makepyfile(
+        test_reusable_network="\n".join(
+            (
+                "def test_session_1(redis_network):",
+                "    assert redis_network.name == 'test_reusable_conflicting_container'",
+            )
+        )
+    )
+
+    result = pytester.runpytest("--reuse-containers")
+    result.assert_outcomes(passed=1)
+
+    # At this stage our network exists and we are going to create a container that
+    # *isn't managed by pytest-docker-tools*
+    # We should refuse to delete that container even when the network is stale
+    docker_client.images.pull(repository="memcached:latest")
+    docker_client.containers.create(
+        name="test_reusable_conflicting_container_net",
+        image="memcached:latest",
+        network="test_reusable_conflicting_container",
+    )
+
+    # Add a label to the network to make the network stale
+    pytester.makeconftest(
+        "\n".join(
+            (
+                "from pytest_docker_tools import container, fetch, network",
+                "redis_network = network(",
+                "    name='test_reusable_conflicting_container',",
+                "    labels={'my-label': 'testtesttest'},",
+                ")",
+            )
+        )
+    )
+
+    result = pytester.runpytest("--reuse-containers")
+    result.assert_outcomes(passed=0, errors=1)
+    result.stdout.re_match_lines([".*is connected to a non-reusable container.*"])
