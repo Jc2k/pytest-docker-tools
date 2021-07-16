12d167b908bcdd441dff89bd9b90dfda5c82b408
Alexander Petrenz <petrenz.a@mail.com>
2021-07-16 23:01:49 +0200

    removed obsolete fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,3 @@
 import pytest
 
 pytest_plugins = ["pytester"]
-
-
-@pytest.fixture()
-def enable_container_reuse(request):
-    request.config.option.reuse_containers = True
-    yield
-    request.config.option.reuse_containers = False
