2277d4beff5b5a479166a446d0d21510f762136e
Alexander Petrenz <petrenz.a@gmail.com>
2021-07-19 17:36:37 +0200

    added type hints to exceptions.py

--- pytest_docker_tools/exceptions.py
+++ pytest_docker_tools/exceptions.py
@@ -1,9 +1,14 @@
+from typing import Any
+
+from docker.models.containers import Container
+
+
 class TimeoutError(Exception):
     pass
 
 
 class ContainerError(Exception):
-    def __init__(self, container, *args, **kwargs):
+    def __init__(self, container: Container, *args: Any, **kwargs: Any) -> None:
         self._container = container
         super().__init__(*args, **kwargs)
 
